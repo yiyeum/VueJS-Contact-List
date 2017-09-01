f7f5570460a703a88a6dcea6fc518211b8c2e318
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-01 16:01:51 -0700

    Validation modified & Contact list style

--- src/Form.vue
+++ src/Form.vue
@@ -1,32 +1,30 @@
 <template>
-    <div class="contactForm">
-        <div class="alert alert-warning text-center" role="alert" v-show="!showAlert">
-            <strong>Hey!</strong> {{alertMessage}}
-        </div>
-        <div v-for="list in contacts">
-            <ul>
-                <li>{{list}}</li>
-            </ul>
-        </div>
-        <div class="row">
-            <form class="col-md-3 contactForm" >
-                <div class="form-group">
-                    <label for="name">Full Name</label>
-                    <input type="text" class="form-control" id="name" placeholder="Enter Name" v-model="name">
-                </div>
-                <div class="form-group">
-                    <label for="phone">Phone</label>
-                    <input type="tel" class="form-control" id="phone" placeholder="Enter Phone number" v-model="phone">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter Email" v-model="email">
-                </div>
-                 <div class="text-center">
-                    <button type="button" class="btn btn-info" v-on:click.prevent="addContact">Add</button>
-                </div>
-            </form>
-        </div>
+    <div>
+            <div class="alert alert-warning text-center" role="alert" v-show="!showAlert">
+                <strong>Hey!</strong> {{alertMessage}}
+            </div>
+            <div class="displayContacts text-center">
+                <p v-for="list in contacts">{{list}}</p>
+            </div>
+            <div class="row">
+                <form class="col-md-3 contactForm" >
+                    <div class="form-group">
+                        <label for="name">Full Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter Name" v-model="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">Phone</label>
+                        <input type="tel" class="form-control" id="phone" placeholder="Enter Phone number" v-model="phone">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Enter Email" v-model="email">
+                    </div>
+                     <div class="text-center">
+                        <button type="button" class="btn btn-info" v-on:click.prevent="addContact">Add</button>
+                    </div>
+                </form>
+            </div>
     </div>
 </template>
 
@@ -62,33 +60,12 @@
                     this.showAlert = true;
                 } // if
 
-                else if(this.name == '' && this.phone =='' && this.email ==''){
+                else if(this.name == '' || this.phone =='' || this.email ==''){
 
                     this.alertMessage = "Please fill the inputs."
                     this.showAlert = false;
 
                 } // else if - three inputs are empty
-
-                else if(this.name == ''){
-
-                    this.alertMessage = "Please fill the name."
-                    this.showAlert = false;
-
-                } // else if - name is empty
-
-                else if(this.phone == ''){
-
-                    this.alertMessage = "Please fill the phone number."
-                    this.showAlert = false;
-
-                } // else if - phone is empty
-
-                else if(this.email == ''){
-
-                    this.alertMessage = "Please fill the email."
-                    this.showAlert = false;
-
-                } // else if - email is empty
 
             } // addContact()
 
@@ -99,5 +76,11 @@
 <style>
     .contactForm{
         margin: 3% auto;
+    }
+    .displayContacts{
+        margin-top: 3%;
+        margin-bottom: 5%;
+        font-size: 21px;
+        font-weight: 200;
     }
 </style>
