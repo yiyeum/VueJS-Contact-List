90916f4cd7d3f419ddb4283b9381e103ac1c2395
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-01 01:21:55 -0700

    Added Components to the page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+import Vue from 'vue'
+import Header from './Header.vue'
+import Form from './Form.vue'
+import ContactList from './ContactList.vue'
+import Footer from './Footer.vue'
 
 new Vue({
-  el: '#app'
+    el: '#app',
+    components:{
+        'kt-header':Header,
+        'kt-form':Form,
+        'kt-list':ContactList,
+        'kt-footer':Footer
+    }
 
 });
